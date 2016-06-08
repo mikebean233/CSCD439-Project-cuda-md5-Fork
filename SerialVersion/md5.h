75a105155013e75072b264313998e47dbcace450
kchumbley95 <kchumbley95@cslinux.(none)>
2016-06-08 15:08:42 -0700

    added md5.c and md5.h

---
+++ SerialVersion/md5.h
@@ -0,0 +1,12 @@
+//
+// Created by Kevin Chumbley on 6/8/16.
+//
+
+#ifndef CSCD539FINALPROJECT_MD5_H
+#define CSCD539FINALPROJECT_MD5_H
+
+#define uint unsigned int
+
+void md5_vfy(unsigned char* data, uint length, uint *a1, uint *b1, uint *c1, uint *d1);
+
+#endif //CSCD539FINALPROJECT_MD5_H
