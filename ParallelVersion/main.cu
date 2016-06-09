3ec4656a467265eeedb0ecc5e627208b42d89ade
Michael Peterson <mikebean233@yahoo.com>
2016-06-09 13:13:04 -0700

    no message

--- ParallelVersion/main.cu
+++ ParallelVersion/main.cu
@@ -66,7 +66,7 @@ int main( int argc, char** argv)
 	int verboseMode = 0;
 	int paramIdx = 0;
 
-	for(; paramIdx < argc; ++i){
+	for(; paramIdx < argc; ++paramIdx){
 		if(strcmp(argv[paramIdx], "-v") == 0)
 			verboseMode = 1;
 		if(strcmp(argv[paramIdx], "-s") == 0)
